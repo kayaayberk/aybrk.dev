570852129ced2259e5e1c41af58679cc38211161
Ayberk Kaya <kayaayberk98@gmail.com>
2024-01-24 23:49:20 +0100

    feat: project page build

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,10 +1,13 @@
-
-
+import ProjectCard from '@/components/ProjectCard';
+import ReachedBottom from '@/components/ReachedBottom';
 
 function page() {
   return (
-    <div></div>
-  )
+    <div className='container mx-auto flex h-full max-w-xl flex-col items-start gap-10 pt-40 md:max-w-4xl md:pt-24'>
+      <ProjectCard />
+      <ReachedBottom />
+    </div>
+  );
 }
 
-export default page
+export default page;
