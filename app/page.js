c2e1daf0c12076157159c139b7ff8b87a3072507
Ayberk Kaya <kayaayberk98@gmail.com>
2024-01-24 23:48:56 +0100

    Modularise and update styles

--- app/page.js
+++ app/page.js
@@ -1,33 +1,35 @@
 // Component Imports
 import Step from '@/components/Step';
 import Card from '@/components/Card';
+import Direct from '@/components/Direct';
 import TechStack from '@/components/TechStack';
 import TextReveal from '@/components/TextReveal';
 import ShowScroll from '@/components/ShowScroll';
 import Description from '@/components/Description';
 import LandingImage from '@/components/LandingImage';
 import StickyAboutMe from '@/components/StickyAboutMe';
+import ReachedBottom from '@/components/ReachedBottom';
 // Constant Imports
-import { PARAGRAPH, STICKY_ABOUT_ME } from '@/lib/constants';
+import { PARAGRAPH, READY_TO_CONTRIBUTE } from '@/lib/constants';
 
 export default function Home() {
   return (
-    <div className='container mx-auto flex h-full max-w-xl flex-col items-start gap-10 pt-32 md:max-w-4xl md:pt-24'>
-      <div className='flex flex-col items-center rounded-2xl md:grid md:grid-cols-2 md:items-start md:gap-10 md:shadow-none'>
-        
+    <div className='container mx-auto flex h-full max-w-xl flex-col items-start gap-10 pt-28 md:max-w-4xl md:pt-24'>
+      <div className='flex flex-col items-center rounded-2xl md:grid md:grid-cols-2 gap-10 md:shadow-none'>
+        <div className='w-full md:grid md:grid-cols-2 gap-10 md:col-span-2'>
         <LandingImage />
 
-        <div className='grid grid-rows-2 gap-10'>
-          <Card>
-            <Description />
-          </Card>
+        <Card hoverActive>
+          <Description />
+        </Card>
+        </div>
+        <div className='col-span-2'>
           <TechStack />
         </div>
-
       </div>
-      <ShowScroll />
 
-      <div className='flex flex-col gap-12'>
+      <ShowScroll />
+      <div className='flex flex-col'>
         <TextReveal
           revealPosition={'start 0.5'}
           revealSpeed={'end start'}
@@ -35,13 +37,16 @@ export default function Home() {
         />
         <Step />
       </div>
-
-      <div className='h-screen w-full'>
-        <StickyAboutMe />
-        <div className='h-screen w-full'></div>
-        <div className='h-screen w-full'></div>
+      <StickyAboutMe />
+      <div className='flex min-h-[700px] w-full flex-col justify-between pb-24 pt-20 md:min-h-[1000px] md:pb-7'>
+        <TextReveal
+          revealPosition={'start 0.5'}
+          revealSpeed={'end start'}
+          textId={READY_TO_CONTRIBUTE}
+        />
+        <Direct />
+        <ReachedBottom />
       </div>
-
     </div>
   );
 }
