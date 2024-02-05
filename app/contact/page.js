fc41eb9974f7a87e53d18b63d86a2e3c791c9e80
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-05 18:02:38 +0100

    feat: introduce contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,10 +1,36 @@
+import Direct from '@/components/Direct';
+import PageHeader from '@/components/PageHeader';
+import ContactForm from '@/components/ContactForm';
+import { Lightbulb } from 'lucide-react';
 
-
-
-function page() {
+function Contact() {
   return (
-    <div></div>
-  )
+    <div className='container mx-auto flex h-full max-w-xl flex-col items-start gap-10 pb-24 pt-28 md:max-w-4xl md:pt-24'>
+      <div className='mx-auto w-full'>
+        <PageHeader />
+      </div>
+      <ContactForm />
+      <div className='mx-auto'>
+        <p className='flex text-xs items-center font-light md:text-sm'>
+          <span className='mr-1 flex items-center font-bold'>
+            <Lightbulb size={20} />
+            Note:
+          </span>
+          Your message will directly be sent as an email to me.
+        </p>
+      </div>
+      <div className='flex min-h-[400px] w-full flex-col justify-center'>
+        <Direct
+          option1='Projects'
+          href1='/projects'
+          option2='Journey'
+          href2='/journey'
+          message1='Check out my projects'
+          message2='Visit my journey page'
+        />
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default Contact;
