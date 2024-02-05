45a97aab66234510963a503b84ede176b698a6f2
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-05 18:07:18 +0100

    feat: page components

---
+++ components/PageHeader.jsx
@@ -0,0 +1,51 @@
+'use client';
+
+import useStore from '@/lib/store';
+import { PAGES } from '@/lib/constants';
+import { usePathname } from 'next/navigation';
+import { cloneElement, useEffect, useRef } from 'react';
+
+function PageHeader() {
+  const pathname = usePathname();
+
+  const fadeRef = useRef(null);
+  const setFadeRef = useStore((state) => state.setFadeRef);
+
+  const formattedPathname =
+    pathname.replace('/', '').charAt(0).toUpperCase() +
+    pathname.replace('/', '').slice(1);
+  const currentPage = PAGES.find((page) => page.label === formattedPathname);
+  const clonedElement = cloneElement(currentPage.icon, { size: 24 });
+
+  useEffect(() => {
+    setFadeRef(fadeRef);
+  });
+
+  return (
+    <div ref={fadeRef} className='flex w-full flex-col gap-6'>
+      {currentPage && (
+        <>
+          <div className='flex items-center gap-2'>
+            <h1 className='text-3xl font-semibold'>{currentPage.label}</h1>
+            {clonedElement}
+          </div>
+          <p className='text-base font-normal'>
+            {currentPage.description &&
+              currentPage.description.map((part, index) =>
+                typeof part === 'string' ? (
+                  part
+                ) : (
+                  <span className='font-semibold' key={index}>
+                    {part.text}
+                  </span>
+                ),
+              )}
+          </p>{' '}
+          <hr className='w-full border-zinc-800' />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default PageHeader;
---
+++ components/StepPointer.jsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { PlusCircle } from 'lucide-react';
+
+function StepPointer({ index, posts }) {
+  return (
+    <div className='relative'>
+      <div className='absolute -top-0.5 z-10'>
+        <PlusCircle
+          strokeWidth={1}
+          className='size-7 fill-black text-white dark:fill-white dark:text-black'
+        />
+      </div>
+      <div
+        className={[
+          'absolute inset-x-3.5 h-full border-l-[1px] border-gray-300 dark:border-zinc-800',
+          index === posts.length - 1 && 'hidden',
+        ].join(' ')}
+      ></div>
+    </div>
+  );
+}
+
+export default StepPointer;
