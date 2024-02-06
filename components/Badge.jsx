f3ba323cb6fb917b33210e80bdcfdfb8a20c840f
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-06 19:50:43 +0100

    refactor: Style

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import DarkModeToggle from './DarkModeToggle';
-import BadgeImage from '@/public/meImages/me.jpg';
+import BadgeImage from '@/public/meImages/IMG_6045.JPG';
 import React, { useEffect, useState } from 'react';
 
 function Badge() {
@@ -24,18 +24,17 @@ function Badge() {
 
   return (
     <div className='flex items-center justify-between rounded-lg border border-gray-300 p-2 hover:bg-gray-200  dark:border-zinc-800 dark:hover:bg-gray-500/20 lg:bg-spice'>
-      <Link
-        href='/journey'
-        className='inline-flex items-center gap-2 rounded-lg'
-      >
-        <Image
-          src={BadgeImage}
-          alt='Ayberk Kaya'
-          width={40}
-          height={40}
-          loading='lazy'
-          className='rounded-full border'
-        />
+      <Link href='/' className='inline-flex items-center gap-2 rounded-lg'>
+        <div className='size-10 overflow-hidden rounded-full border'>
+          <Image
+            src={BadgeImage}
+            alt='Ayberk Kaya'
+            width={70}
+            height={70}
+            loading='lazy'
+            objectFit='cover'
+          />
+        </div>
         <div className='flex flex-1 flex-col'>
           <span className='font-semibold tracking-tight'>Ayberk Kaya</span>
           <span className='text-xs font-extralight tracking-wide dark:text-card'>
