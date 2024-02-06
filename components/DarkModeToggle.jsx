671f2dafdd0d56f04738e4fd6c4c89daed0f129a
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-06 19:50:58 +0100

    refactor: Add system icon

--- components/DarkModeToggle.jsx
+++ components/DarkModeToggle.jsx
@@ -5,13 +5,13 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { useTheme } from 'next-themes';
-import { Moon, Sun } from 'lucide-react';
+import { Monitor, Moon, Sun } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 
 function DarkModeToggle() {
   const [mounted, setMounted] = useState(false);
-  const {  setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => setMounted(true), []);
 
@@ -23,21 +23,41 @@ function DarkModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger
         asChild
-        className='bg-spice dark:bg-spice shadow-none md:flex'
+        className='bg-spice shadow-none dark:bg-spice md:flex'
       >
-        <Button variant='ghost' size='icon' className='hover:bg-gray-300 dark:hover:bg-gray-500/25'>
-          <Sun className='size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+        <Button
+          variant='ghost'
+          size='icon'
+          className='hover:bg-gray-300 dark:hover:bg-gray-500/25'
+        >
+          {theme === 'light' && (
+            <Sun className='size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          )}
+          {theme === 'dark' && (
+            <Moon className='absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          )}
+          {theme === 'system' && (
+            <Monitor className='size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-0' />
+          )}
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className='dark:bg-spice dark:border-zinc-800'>
-        <DropdownMenuItem onClick={() => setTheme('light')} className='hover:bg-gray-200 dark:hover:bg-gray-500/25'>
+      <DropdownMenuContent className='dark:border-zinc-800 dark:bg-spice'>
+        <DropdownMenuItem
+          onClick={() => setTheme('light')}
+          className='hover:bg-gray-200 dark:hover:bg-gray-500/25'
+        >
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')} className='hover:bg-gray-200 dark:hover:bg-gray-500/25'>
+        <DropdownMenuItem
+          onClick={() => setTheme('dark')}
+          className='hover:bg-gray-200 dark:hover:bg-gray-500/25'
+        >
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')} className='hover:bg-gray-200 dark:hover:bg-gray-500/25'>
+        <DropdownMenuItem
+          onClick={() => setTheme('system')}
+          className='hover:bg-gray-200 dark:hover:bg-gray-500/25'
+        >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
