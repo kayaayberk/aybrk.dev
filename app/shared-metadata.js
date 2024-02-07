fcd834fc56b2f50f0c58ce84a0b0f03a45797462
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-08 00:41:45 +0100

    Update sharedDescription

--- app/shared-metadata.js
+++ app/shared-metadata.js
@@ -1,6 +1,6 @@
 export const sharedTitle = 'Ayberk Kaya';
 export const sharedDescription =
-  'Software Developer, Designer, AI & Tech Enthusiast, based in Warsaw, Poland';
+  "Hi! I'm Ayberk, I'm a Software Developer, Designer and AI & Tech Enthusiast based in Warsaw, Poland";
 export const sharedImage = {
   width: 1200,
   height: 630,
