9d0ebf4dab0bcc7d1313331374229ec1ca220603
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-08 23:57:37 +0100

    Refactor destructuring

--- components/JourneyDescRenderer.jsx
+++ components/JourneyDescRenderer.jsx
@@ -1,15 +1,36 @@
-'use client';
+import Link from 'next/link';
 
 function JourneyDescRenderer({ post }) {
+  const { content } = post;
+
+  const {
+    json: {
+      content: [
+        {
+          content: [
+            { value: description } = {},
+            {
+              data: { uri: link } = {},
+              content: [{ value: linkText } = {}] = [],
+            } = {},
+          ] = [],
+        } = {},
+      ] = [],
+    } = {},
+  } = content ?? {};
+
+  // console.log(post);
   return (
     <div className='flex flex-col '>
-      <span className='text-base font-semibold'>{post.subtitle}</span>
-      <span className='text-sm font-light text-muted-foreground'>
-        {post.content.json.content[0].content.length >= 1
-          ? post.content.json.content[0].content.map((item) => {
-              return item.value;
-            })
-          : post.content.json.content[0].content[0].value}
+      <span className=' font-semibold'>{post.subtitle}</span>
+      <span className='text-sm font-light'>
+        {description}{' '}
+        <Link
+          className='text-blue-500 underline-offset-4 hover:text-blue-600 hover:underline'
+          href={link ? link : ''}
+        >
+          {linkText ? linkText : ''}
+        </Link>
       </span>
     </div>
   );
