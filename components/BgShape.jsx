765fdb99e8a180166dff5e169eb61f74f6700c7c
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-09 23:06:58 +0100

    Fix mobile screen resizing

--- components/BgShape.jsx
+++ components/BgShape.jsx
@@ -3,7 +3,7 @@
 function BgShape() {
   return (
     <>
-      <div className='absolute inset-0 -z-50 hidden h-[200px] w-[350px] rotate-45 -skew-y-12 rounded-full bg-gradient-to-tr from-purple-700 to-cyan-500 opacity-30 blur-[80px] md:blur-[300px] filter dark:flex md:h-[800px] md:w-[1000px]'></div>
+      <div className='absolute right-0 top-0 -z-50 hidden h-[50px] w-[750px] rotate-45 -skew-y-12 rounded-full bg-gradient-to-tr from-purple-700 to-cyan-500 opacity-30 blur-[80px] filter dark:flex md:h-[800px] md:w-[1000px] md:blur-[300px]'></div>
     </>
   );
 }
