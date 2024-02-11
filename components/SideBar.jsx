57c2f219711bcf5aed37ae962d3c6f9b58e8f561
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-11 20:53:58 +0100

    Add scroll to SideBar

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -2,7 +2,7 @@
 
 function SideBar({ children }) {
   return (
-    <div className="hidden sticky left-0 lg:w-[270px] xl:w-72 lg:flex lg:flex-col border-r border-r-muted-foreground/25 dark:bg-spice">
+    <div className="hidden sticky left-0 lg:w-[270px] xl:w-72 lg:flex lg:flex-col border-r border-r-muted-foreground/25 dark:bg-spice overflow-scroll">
       {children}
     </div>
   );
