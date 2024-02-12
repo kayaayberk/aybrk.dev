49e9dfc6a425818dc4ad19e05b9b5de9fca3bc57
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-13 00:11:47 +0100

    Fix text gradient

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-function Card({ children, classId, hoverActive }) {
+function Card({ children, classId }) {
   const className = [
     'w-full flex flex-col gap-4 p-4 text-start transition-all bg-gray-300/10 dark:bg-gray-400/5 filter backdrop-blur-3xl border border-muted-foreground/10 dark:border-muted-foreground/10 shadow-xl dark:shadow-black/40 ease-in md:rounded-lg animate-slide opacity-0',
     classId === 'tech-stack' ? 'rounded-xl' : 'rounded-b-gl',
