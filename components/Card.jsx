1627705f899e38dda5448501a76ef347d1cab8ec
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-13 13:21:44 +0100

    Refactor border radius

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,11 @@
 'use client';
 
 function Card({ children }) {
-  const className = [
-    'w-full flex flex-col gap-4 p-4 text-start transition-all bg-gray-300/10 dark:bg-gray-400/5 filter backdrop-blur-3xl border border-muted-foreground/10 dark:border-muted-foreground/10 shadow-xl dark:shadow-black/40 ease-in md:rounded-lg animate-slide opacity-0',
-  ].join(' ');
-
-  return <div className={className}>{children}</div>;
+  return (
+    <div className='flex w-full animate-slide flex-col gap-4 border border-muted-foreground/10 bg-gray-300/10 p-4 text-start opacity-0 shadow-xl filter backdrop-blur-3xl transition-all ease-in dark:border-muted-foreground/10 dark:bg-gray-400/5 dark:shadow-black/40 rounded-lg'>
+      {children}
+    </div>
+  );
 }
 
 export default Card;
