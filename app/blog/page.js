5c3ba683603a70c206af5a1f75ea73e8a3369040
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-20 02:04:40 +0100

    Remove unnecessary imports

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,28 +1,14 @@
-import { getPostTitle } from '@/lib/contentful';
-import PageHeader from '@/components/PageHeader';
 import { sharedTitle, sharedDescription } from '@/app/shared-metadata';
 
-async function fetchData() {
-  const data = await getPostTitle();
-  return data;
-}
-
-async function Blog() {
-  const result = await fetchData();
-  const { title, slug } = result[0];
-  console.log(title, slug);
-
+export default async function Blog() {
   return (
     <>
-      <PageHeader />
       <div className='container mx-auto flex h-full max-w-xl flex-col items-start gap-10 pb-24 pt-10 md:max-w-4xl'>
-        {/* <p className='text-sm'>{title}</p> */}
+        asdas
       </div>
     </>
   );
 }
-
-export default Blog;
 
 export async function generateMetadata() {
   const url = '/blog';
