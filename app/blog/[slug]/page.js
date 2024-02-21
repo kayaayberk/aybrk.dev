985c1813761acd4b6d48751d2b9c1435b7995a11
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-21 18:23:34 +0100

    Add dynamic pages

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,39 @@
+import { draftMode } from 'next/headers';
+import { notFound } from 'next/navigation';
+import { isDevelopment } from '@/lib/utils';
+import PageHeader from '@/components/PageHeader';
+import { RichText } from '@/components/RichText';
+import { getAllPostSlugs, getPost } from '@/lib/contentful';
+
+export async function generateStaticParams() {
+  const allPosts = await getAllPostSlugs();
+  return allPosts.map((post) => ({ params: { slug: post.slug } }));
+}
+
+async function fetchData(slug) {
+  const { isEnabled } = draftMode();
+  const data = await getPost(slug, isDevelopment ? true : isEnabled);
+  if (!data) notFound();
+  return { data };
+}
+async function fetchPost() {
+  const allPosts = await getAllPostSlugs();
+  return { allPosts };
+}
+
+export default async function BlogSlug({ params }) {
+  const { slug } = params;
+  const { allPosts } = await fetchPost();
+  const {
+    data: { title, body, createdAt },
+  } = await fetchData(slug);
+
+  return (
+    <>
+      <PageHeader allPosts={allPosts} title={title} createdAt={createdAt} />
+      <div className='container mx-auto flex min-h-max max-w-xl flex-col items-start gap-10 pb-24 md:max-w-2xl'>
+        <RichText body={body} />
+      </div>
+    </>
+  );
+}
