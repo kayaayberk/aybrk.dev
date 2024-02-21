c597c4d4ec8da5b5d63c9c535f4a3216b8cabeee
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-22 00:32:58 +0100

    Refactor render conditions

--- components/SideBarContent.jsx
+++ components/SideBarContent.jsx
@@ -8,7 +8,6 @@ import BuiltWith from './BuiltWith';
 import PageBlogPostList from './PageBlogPostList';
 
 function SideBarContent({ handleDrawerClose, allPosts }) {
-  console.log(allPosts);
   return (
     <div className='flex h-screen w-full flex-col gap-4 px-1 py-3  text-sm '>
       <div className='flex flex-1 flex-col justify-start gap-4 px-2'>
@@ -16,16 +15,12 @@ function SideBarContent({ handleDrawerClose, allPosts }) {
         <Pages handleDrawerClose={handleDrawerClose} />
         <Online handleDrawerClose={handleDrawerClose} />
         <Info />
-        {allPosts.lenght && (
-          <div>
-            <>
-              <span className='px-2 text-xs font-normal leading-relaxed text-card dark:text-card'>
-                Latest Posts
-              </span>
-              <PageBlogPostList allPosts={allPosts} />
-            </>
-          </div>
-        )}
+        <div>
+          <span className='px-2 text-xs font-normal leading-relaxed text-card dark:text-card'>
+            Latest Posts
+          </span>
+          <PageBlogPostList allPosts={allPosts} />
+        </div>
         <BuiltWith handleDrawerClose={handleDrawerClose} />
 
         {/* Add heart counter */}
