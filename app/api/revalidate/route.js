2120cb2976046e2646612d8ec662696f9279c9b6
Ayberk Kaya <kayaayberk98@gmail.com>
2024-02-23 00:00:16 +0100

    Create revalidation route handler

---
+++ app/api/revalidate/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
+
+export const runtime = 'edge';
+
+export async function GET(request) {
+  const searchParams = request.nextUrl.searchParams;
+  const secret = searchParams.get('secret');
+  console.log(secret);
+
+  if (secret !== process.env.NEXT_REVALIDATE_SECRET) {
+    return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
+  }
+
+  const path = searchParams.get('path') || '/';
+  revalidatePath(path);
+  return NextResponse.json(
+    {
+      revalidated: true,
+      message: `Revalidation request for path: ${path} is successful`,
+      now: Date.now(),
+    },
+    { status: 200 },
+  );
+}
