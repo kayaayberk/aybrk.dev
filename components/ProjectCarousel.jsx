8b2269aaa0e915d6cbe27a97e2fa71f387a54e45
Ayberk Kaya <kayaayberk98@gmail.com>
2024-03-08 21:01:23 +0100

    feat: project card carousel for home page

---
+++ components/ProjectCarousel.jsx
@@ -0,0 +1,66 @@
+'use client';
+
+import {
+  Carousel,
+  CarouselItem,
+  CarouselNext,
+  CarouselContent,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import Card from './Card';
+import Link from 'next/link';
+import { useState } from 'react';
+import { PROJECTS } from '@/lib/constants';
+import Autoplay from 'embla-carousel-autoplay';
+import ProjectCardContent from './ProjectCardContent';
+
+function ProjectCarousel({ carousel }) {
+  const [isHovered, setIsHovered] = useState(false);
+  console.log(isHovered)
+
+  const handleMouseEnter = () => {
+    setIsHovered((prev) => !prev);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered((prev) => !prev);
+  };
+
+  return (
+    <Card
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      isHovered={isHovered}
+      carousel
+      className={`hidden lg:block`}
+    >
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 15000,
+          }),
+        ]}
+      >
+        <Link href='/projects'>
+          <CarouselContent>
+            {PROJECTS.map((project, index) => {
+              return (
+                <CarouselItem key={index}>
+                  <ProjectCardContent
+                    key={project.title}
+                    project={project}
+                    carousel={carousel}
+                    isHovered={isHovered}
+                  />
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+        </Link>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </Card>
+  );
+}
+
+export default ProjectCarousel;
