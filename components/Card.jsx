0703c66c50fc25cd27212dfe92823ecb17e70b6e
Ayberk Kaya <kayaayberk98@gmail.com>
2024-03-08 21:02:09 +0100

    refactor: adjust styles and props for new components

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,10 @@
 'use client';
 
-function Card({ children, desc }) {
+function Card({ children, desc, className, carousel, isHovered, ...props }) {
   return (
     <div
-      className={`flex w-full animate-slide flex-col gap-4 border border-muted-foreground/10 bg-gray-300/10 ${desc ? 'delay-0' : 'delay-250'} p-4 rounded-lg text-start opacity-0 shadow-xl filter backdrop-blur-3xl transition-all ease-in dark:border-muted-foreground/10 dark:bg-gray-400/5 dark:shadow-black/40`}
+      className={`${(className, carousel ? `dark:shadow-black/40 dark:hover:dark:shadow-[0_0_20px_rgba(154,_120,_255)]` : 'dark:shadow-black/40')} flex w-full animate-slide flex-col gap-4 border border-muted-foreground/10 bg-gray-300/10 ${desc ? 'delay-0' : 'delay-250'} rounded-lg p-4 text-start opacity-0 shadow-xl filter backdrop-blur-3xl transition-transform ease-in dark:border-muted-foreground/10 dark:bg-gray-400/5`}
+      {...props}
     >
       {children}
     </div>
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -4,13 +4,13 @@ import Card from './Card';
 import { PROJECTS } from '@/lib/constants';
 import ProjectCardContent from './ProjectCardContent';
 
-function ProjectCard() {
+function ProjectCard({carousel}) {
   return (
     <div className='flex flex-col gap-10 animate-slide'>
       {PROJECTS.map((project, index) => {
         return (
           <Card key={project.title}>
-            <ProjectCardContent project={project} />
+            <ProjectCardContent project={project} carousel={carousel} />
           </Card>
         );
       })}
