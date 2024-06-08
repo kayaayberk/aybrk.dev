8aed4eb5afe328793402aac3363773087af15233
Ayberk Kaya <kayaayberk98@gmail.com>
2024-06-08 23:45:55 +0200

    styles: edit hover effects on sidebar

--- components/Info.jsx
+++ components/Info.jsx
@@ -11,24 +11,34 @@ function Info() {
       <div className='flex flex-col gap-1'>
         {INFO.map((link) => {
           return !link.mailTo ? (
-            <span key={link.label} className='flex items-center gap-3 px-2 py-1.5 text-sm font-medium tracking-normal dark:text-white/80'>
+            <span
+              key={link.label}
+              className='flex items-center gap-3 px-2 py-1.5 text-sm font-medium tracking-normal dark:text-white/80'
+            >
               {link.icon}
-              <span className='tracking-normal font-normal'>{link.label}</span>
+              <span className='font-normal tracking-normal'>{link.label}</span>
             </span>
           ) : (
             <Link
               key={link.label}
-              className='flex items-center justify-between rounded-md px-2 py-1.5 hover:bg-gray-200 dark:hover:bg-gray-500/20'
+              className='group flex items-center justify-between rounded-md px-2 py-1.5 hover:bg-gray-200 dark:hover:bg-gray-500/20'
               href='mailto:kayaayberk98@gmail.com'
             >
               <span className='hover flex items-center gap-3 text-sm font-normal'>
                 {link.icon}
-                <span className='tracking-normal font-normal dark:text-white/80'>
+                <span className='font-normal tracking-normal dark:text-white/80'>
                   {link.label}
                 </span>
               </span>
               <span className='dark:text-white/80'>
-                {<ArrowUpRight size={16} className='p-0' />}
+                <ArrowUpRight
+                  size={16}
+                  className='-translate-x-3 translate-y-3 text-gray-400 opacity-0 transition-all duration-300 group-hover:-translate-x-0 group-hover:translate-y-0 group-hover:opacity-100 dark:text-white/50'
+                />
+                <ArrowUpRight
+                  size={16}
+                  className='absolute -translate-y-4 text-gray-400 transition-all duration-300 group-hover:-translate-y-7 group-hover:translate-x-3 group-hover:opacity-0 dark:text-white/50 '
+                />
               </span>
             </Link>
           );
--- components/NavigationLink.jsx
+++ components/NavigationLink.jsx
@@ -5,6 +5,7 @@ import { memo } from 'react';
 import { ArrowUpRight } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
 import { useKeyPress } from '@/hooks/useKeyPress';
+import { cn } from '@/lib/utils';
 
 export const NavigationLink = memo(
   ({ href, label, icon, title, handleDrawerClose, scrollToTop, number }) => {
@@ -50,31 +51,40 @@ export const NavigationLink = memo(
         rel={title ? 'noopener noreferrer' : ''}
         onClick={scrollToTop || handleDrawerClose}
         scroll
-        className={[
-          'flex items-center justify-between rounded-md px-2 py-1.5 pr-1 ',
+        className={cn(
+          'group flex items-center justify-between overflow-hidden rounded-md px-2 py-1.5 pr-1',
           isActive
             ? 'bg-gray-400/40 dark:bg-white/20'
             : 'hover:bg-gray-200 dark:text-white/80 dark:hover:bg-gray-500/20',
-        ].join(' ')}
+        )}
       >
         <span className='flex items-center gap-3 font-normal'>
           {icon}
           <span className='font-normal tracking-normal'>{label}</span>
         </span>
-        <span>
+        <span className='overflow-hidden'>
           {!title ? (
             <span
               className={[
                 'hidden size-5 items-center justify-center rounded-sm px-1 text-xs md:flex',
                 isActive
                   ? 'text-black dark:text-white'
-                  : 'bg-black/5 text-gray-400 dark:bg-white/10 dark:text-white/50',
+                  : 'text-gray-400 dark:text-white/50',
               ].join(' ')}
             >
               {number}
             </span>
           ) : (
-            <ArrowUpRight size={16} />
+            <>
+              <ArrowUpRight
+                size={16}
+                className='-translate-x-3 translate-y-3 text-gray-400 opacity-0 transition-all duration-300 group-hover:-translate-x-0 group-hover:translate-y-0 group-hover:opacity-100 dark:text-white/50'
+              />
+              <ArrowUpRight
+                size={16}
+                className='absolute -translate-y-4 text-gray-400 transition-all duration-300 group-hover:-translate-y-7 group-hover:translate-x-3 group-hover:opacity-0 dark:text-white/50 '
+              />
+            </>
           )}
         </span>
       </Link>
